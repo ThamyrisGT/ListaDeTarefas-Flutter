4b4ee84d0e3d94e7fd05edb4ca7c8fd6c3945e53
Thamyris <tgthurler@gmail.com>
2021-09-22 21:31:24 -0300

    Implementado função de excluir e ao recarregar a página ordenar a lista das tarefas por finalizada ou não

--- lib/main.dart
+++ lib/main.dart
@@ -17,18 +17,48 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-final _toDoController = TextEditingController();
+  final _toDoController = TextEditingController();
 
   List _toDoList = [];
 
-  void _addToDo(){
-  setState(() {
-      Map<String,dynamic> newToDo = Map();
-    newToDo ["title"] =_toDoController.text;
-    _toDoController.text="";
-    newToDo["ok"] =false;
-    _toDoList.add(newToDo);
-  });
+  late Map<String, dynamic> _lastRemoved;
+  late int _lastRemovedPos;
+
+  @override
+  void initState() {
+    super.initState();
+    _readData().then((data) {
+      setState(() {
+        _toDoList = json.decode(data);
+      });
+    });
+  }
+
+  void _addToDo() {
+    setState(() {
+      Map<String, dynamic> newToDo = Map();
+      newToDo["title"] = _toDoController.text;
+      _toDoController.text = "";
+      newToDo["ok"] = false;
+      _toDoList.add(newToDo);
+      _saveData();
+    });
+  }
+
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {
+      _toDoList.sort((a, b) {
+        if (a["ok"] && !b["ok"])
+          return 1;
+        else if (!a["ok"] && b["ok"])
+          return -1;
+        else
+          return 0;
+      });
+      _saveData();
+    });
+    return null;
   }
 
   @override
@@ -57,33 +87,77 @@ final _toDoController = TextEditingController();
                     color: Colors.blueAccent,
                     child: Text("ADD"),
                     textColor: Colors.white,
-                    onPressed:_addToDo,
+                    onPressed: _addToDo,
                   ),
                 ],
               ),
             ),
             Expanded(
-              child: ListView.builder(
-                  padding: EdgeInsets.only(top: 10),
-                  itemCount: _toDoList.length,
-                  itemBuilder: (context, index) {
-                    return CheckboxListTile(
-                      title: Text(_toDoList[index]["title"]),
-                      value: _toDoList[index]["ok"],
-                      secondary: CircleAvatar(
-                        child: Icon(
-                          _toDoList[index]["ok"]?
-                          Icons.check : Icons.error,
-                        ),),
-                      onChanged: (check) { 
-                        setState(() {
-                          _toDoList[index]["ok"] =check;
-                        });
-                       },);
-                  }),
+              child: RefreshIndicator(
+                onRefresh:_refresh,
+                child: ListView.builder(
+                    padding: EdgeInsets.only(top: 10),
+                    itemCount: _toDoList.length,
+                    itemBuilder: buildItem),
+              ),
             ),
           ],
         ));
+  }
+
+  Widget buildItem(context, index) {
+    return Dismissible(
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(
+            Icons.delete,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        secondary: CircleAvatar(
+          child: Icon(
+            _toDoList[index]["ok"] ? Icons.check : Icons.error,
+          ),
+        ),
+        onChanged: (check) {
+          setState(() {
+            _toDoList[index]["ok"] = check;
+            _saveData();
+          });
+        },
+      ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+          _saveData();
+
+          final snack = SnackBar(
+            content: Text("Tarefa ${_lastRemoved["title"]} removida!"),
+            action: SnackBarAction(
+                label: "Desfazer",
+                onPressed: () {
+                  setState(() {
+                    _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                    _saveData();
+                  });
+                }),
+            duration: Duration(seconds: 5),
+          );
+          Scaffold.of(context).removeCurrentSnackBar(); 
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
+    );
   }
 
   Future<File> _getFile() async {
